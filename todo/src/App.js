c8ac65f02813b49bc179f567e46df9aa56843b48
dilaraerdem <dilaraerdem845@gmail.com>
2023-04-05 23:38:30 +0300

    yeniden

--- todo/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useTodoLayerValue } from "./context/TodoContext";
-
-const App = () => {
-  const [{ todos }, dispatch] = useTodoLayerValue;
-  return <div>App</div>;
-};
-
-export default App;
